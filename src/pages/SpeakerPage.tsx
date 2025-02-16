bffb811595f9a0c4a1fd70e17666cc5b5ae53abf
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-16 17:16:59 +0000

    Update SpeakerPage.tsx

--- src/pages/SpeakerPage.tsx
+++ src/pages/SpeakerPage.tsx
@@ -2,26 +2,43 @@ import { useState } from 'react';
 import { GridItem } from '../components/GridItem';
 import { speakers } from '../utils';
 import { StyledDiv, StyledGrid, StyledPageContainer } from './SpeakerPage.styled';
+import { motion } from 'framer-motion';
 
 export const SpeakerPage: React.FC = () => {
   const [ selectedItem, setSelectedItem ] = useState<string | null>(null);
 
+  const cardVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: { delay: index * 0.1, duration: 0.5, ease: 'easeOut' },
+    }),
+  };
+
   return (
-    <>
       <StyledPageContainer>
         <h1>Speakers</h1>
         <StyledGrid>
-          {speakers.map((speaker) => (
-            <GridItem
+          {speakers.map((speaker, index) => (
+            <motion.div
               key={speaker.id}
-              {...speaker}
-              isSelected={selectedItem === speaker.id}
-              setSelectedItem={setSelectedItem}
-            />
+              custom={index}
+              variants={cardVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <GridItem
+                key={speaker.id}
+                {...speaker}
+                isSelected={selectedItem === speaker.id}
+                setSelectedItem={setSelectedItem}
+              />
+              </motion.div>
           ))}        
         </StyledGrid>
+        <StyledDiv />
       </StyledPageContainer>
-      <StyledDiv />
-    </>
   );
 };
