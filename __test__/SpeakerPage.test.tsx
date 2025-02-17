5e65a0df598f23bb8758ea4a2c074b39b930dd2b
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-17 14:50:03 +0000

    Add tests for SpeakerPage

---
+++ __test__/SpeakerPage.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { SpeakerPage } from '../src/pages/SpeakerPage';
+import data from '../src/events.json';
+import { renderWithProviders } from './test-utils'; 
+
+
+describe('SpeakerPage component', () => {
+  it('renders the page title', () => {
+    renderWithProviders(<SpeakerPage />);
+    const titleElement = screen.getByText(/Talk Schedule/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('renders tables for Talk Room 1 and Talk Room 2', () => {
+    renderWithProviders(<SpeakerPage />);
+    const talkRoom1Heading = screen.getByText(/Talk Room 1/i);
+    const talkRoom2Heading = screen.getByText(/Talk Room 2/i);
+    expect(talkRoom1Heading).toBeInTheDocument();
+    expect(talkRoom2Heading).toBeInTheDocument();
+  });
+});
+
