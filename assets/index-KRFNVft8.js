dd090fcd1f87b6a34849d276fce94decd1427349
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-18 15:18:48 +0000

    Updates

--- assets/index-KRFNVft8.js
+++ assets/index-KRFNVft8.js
@@ -76,8 +76,7 @@ Please change the parent <Route path="${F}"> to <Route path="${F==="/"?"*":`${F}
   transition: background-color 0.3s ease, border-color 0.3s ease,
     box-shadow 0.3s ease;
 
-  &:hover,
-  &:focus {
+  &:hover {
     background-color: ${l.colors.lilac};
     border: 2px solid ${l.colors.lilacShade};
     border-top: 1px solid rgba(176, 97, 255, 0.8);
