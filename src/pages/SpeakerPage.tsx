0145a9b9af3129770d8e14ed488a1bf7ad5e5363
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-18 16:48:45 +0000

    Remove useEffect code

--- src/pages/SpeakerPage.tsx
+++ src/pages/SpeakerPage.tsx
@@ -23,19 +23,6 @@ export const SpeakerPage = () => {
     'speakers'
   ];
 
-  const isEdge = () => /Edg/.test(navigator.userAgent);
-
-  useEffect(() => {
-    const handlePageShow = (event: PageTransitionEvent) => {
-      if (isEdge() && event.persisted) {
-        window.location.reload();
-      }
-    };
-
-    window.addEventListener('pageshow', handlePageShow);
-    return () => window.removeEventListener('pageshow', handlePageShow);
-  }, []);
-
   return (
     <PageContainer>
       <h1>Talk Schedule</h1>
