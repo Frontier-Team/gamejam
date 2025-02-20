7feea9855ee7cc626700b39118895bda57a81f30
Bradley Morrison <bradley_morrison@outlook.com>
2025-02-20 10:37:09 +0000

    Reinstated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { HomePage } from "./pages/HomePage";
+import MapPage from "./pages/MapPage";
 import { AppThemeProvider } from "./providers/ThemeProvider";
 import { SpeakerPage } from "./pages/SpeakerPage";
 
@@ -10,8 +11,9 @@ export const App = () => {
       <HashRouter>
         <NavBar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/speakers" element={<SpeakerPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/speakers' element={<SpeakerPage />} />
+          <Route path='/map' element={<MapPage />} />
         </Routes>
       </HashRouter>
     </AppThemeProvider>
