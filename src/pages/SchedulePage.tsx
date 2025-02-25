a0a00d30508580ea2700abf7f5b05bfd1952a621
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-02-25 15:25:20 +0000

    Refactor SchedulePage and create a separate Timeline component

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -1,22 +1,8 @@
-import { Card, Connector, Line, TimelineContainer, TimelineItem } from './SchedulePage.styled';
+import { CenteredParagraph, Heading, PageContainer } from '../styles/sharedStyles';
+import { Timeline } from '../components/Timeline';
 import data from '../db.json';
-import { CenteredParagraph, Heading, PageContainer, Paragraph } from '../styles/sharedStyles';
 
 export const SchedulePage = () => {
-  const timelineVariants = {
-    hidden: {
-      opacity: 0,
-      y: 50,
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.6,
-      },
-    }
-  };
-
   return (
     <PageContainer>
       <Heading>
@@ -28,24 +14,7 @@ export const SchedulePage = () => {
       <CenteredParagraph>
         Find below the running order for March 4, 2025.
       </CenteredParagraph>
-      <TimelineContainer>
-        <Line />
-        {data.schedule.length > 0 && data.schedule.map((event, index) => (
-          <TimelineItem
-            key={event.description}
-            side={index % 2 === 0 ? 'left' : 'right'}
-            variants={timelineVariants}
-            initial='hidden'
-            animate='visible'
-          >
-            <Connector side={index % 2 === 0 ? 'left' : 'right'} />
-            <Card>
-              <Paragraph>{event.time}</Paragraph>
-              <Paragraph>{event.description}</Paragraph>
-            </Card>
-          </TimelineItem>
-        ))}
-      </TimelineContainer>
+      <Timeline schedule={data.schedule}/>
     </PageContainer>
   );
 };
